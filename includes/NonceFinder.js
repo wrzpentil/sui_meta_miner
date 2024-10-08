e7e622fafa221c208d7f45412b9d12ab210c950d
WRZ Pentil <fsaputra1602@gmail.com>
2024-10-08 19:23:48 +0700

    Update NonceFinder.js

--- includes/NonceFinder.js
+++ includes/NonceFinder.js
@@ -7,7 +7,7 @@ export default class NonceFinder {
         this._tryingNonces = [];
         this._initialNonce = 0;
         this._startedSearchAt = null;
-        this._workersCount = 120;
+        this._workersCount = 96;
 
         this._askedToStop = false;
 
